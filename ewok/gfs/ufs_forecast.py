24d6f0daca6a4cd448c3844abb469653a6dbac13
jian.kuang <Jian.Kuang@noaa.gov>
2021-03-25 20:22:16 -0500

    add class UfsForecast

---
+++ ewok/gfs/ufs_forecast.py
@@ -0,0 +1,21 @@
+from ewok.suite import Suite
+
+
+class UfsForecast:
+
+    @staticmethod
+    def build():
+        with Suite() as suite:
+
+            with suite.add('setup') as setup:
+                create = setup.add('CreateExperiment')
+                stage = setup.add('Stage', create)
+            start = 'config.init_cycle'
+            stop = 'config.last_cycle'
+            step = 'config.step_cycle'
+            with suite.add('fcCycle', start=start, stop=stop, step=step) as cycle:
+                analysis = cycle.add('GetAnalysis', create)
+                fc = cycle.add('Forecast', setup, analysis, jedi)
+                cycle.add('SaveForecast', fc)
+            suite.add('FinishExperiment', cycle, defstatus='suspended')
+        return suite
