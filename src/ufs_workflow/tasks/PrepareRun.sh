34c79ae31cb5e2e1c8dfc1e9cf3be57bfdbeede2
jikuang <jikuang@umd.edu>
2021-06-10 21:16:46 -0500

    directly put all missing values of ufs.yaml in the experiment file  -- This is suboptimal, will use smarter way to eliminate most of them upgrade PrepareRun.sh, add recursive routine to expand config

--- src/ufs_workflow/tasks/PrepareRun.sh
+++ src/ufs_workflow/tasks/PrepareRun.sh
@@ -1,6 +1,6 @@
 set -eux
 
-eval $(ewok_source_config $CONFIG_FILE current_dir stage_dir)
+eval $(ewok_source_config $CONFIG_FILE current_dir stage_dir suite_dir)
 mkdir -p $current_dir || true
 cp -r $stage_dir/* $current_dir
 cd $current_dir
@@ -13,20 +13,31 @@ import sys
 import os
 from ewok.runtime import Configuration
 from solo.template import TemplateConstants, Template
-config = Configuration(sys.argv[1], silent=True)
-with open('input.nml','r+') as fd:
+config_toplevel = Configuration(sys.argv[1], silent=True)
+
+config = {}
+def expand_config(d):
+    for key, value in d.items():
+        if isinstance(value, dict):
+             expand_config(value)
+        else:
+             config[key] = value
+
+expand_config(config_toplevel)
+
+with open('input.nml','r') as fd:
     temp = fd.read()
-    input_nml = Template.substitute_structure(temp, TemplateConstants.AT_SQUARE_BRACES, config.get)
+    fd.close()
+
+input_nml = Template.substitute_structure(temp, TemplateConstants.AT_SQUARE_BRACES, config.get)
+with open('input.nml','w') as fd:
     fd.write(input_nml)
+    fd.close()
 
-fd.close()
-
-with open('model_configure','r+') as fd:
-    temp = fd.read()
-    input_nml = Template.substitute_structure(temp, TemplateConstants.AT_SQUARE_BRACES, config.get)
-    fd.write(input_nml)
-
-fd.close()
+model_configure = Template.substitute_structure(temp, TemplateConstants.AT_SQUARE_BRACES, config.get)
+with open('model_configure','w') as fd:
+    fd.write(model_configure)
+    fd.close()
 
 EOF
 
