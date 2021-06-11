c969a2b7b1b418596d337f39dfd39bf7059bf863
jikuang <jikuang@umd.edu>
2021-06-10 19:06:10 -0500

    update script

--- src/ufs_workflow/tasks/PrepareRun.sh
+++ src/ufs_workflow/tasks/PrepareRun.sh
@@ -9,14 +9,20 @@ module use /home/jkuang/opt/modulefiles/apps
 module load ufs_test/1.0.0
 
 cat << EOF > conf_var.py
-#!/usr/bin/python3
 import sys
 import os
-from ewok.configuration import processing as ps
+from ewok.runtime import Configuration
 from solo.template import TemplateConstants, Template
 config = Configuration(sys.argv[1], silent=True)
-with open('input.nml','w+') as fd:
-    temp = fd.readlines()
+with open('input.nml','r+') as fd:
+    temp = fd.read()
+    input_nml = Template.substitute_structure(temp, TemplateConstants.AT_SQUARE_BRACES, config.get)
+    fd.write(input_nml)
+
+fd.close()
+
+with open('model_configure','r+') as fd:
+    temp = fd.read()
     input_nml = Template.substitute_structure(temp, TemplateConstants.AT_SQUARE_BRACES, config.get)
     fd.write(input_nml)
 
@@ -24,6 +30,4 @@ fd.close()
 
 EOF
 
-chmod 755 conf_var.py
-
-./conf_var.py
+python conf_var.py $suite_dir/ufs.yaml
