c56539dd69cfb839e09cbaa16eaa42005dedd987
jikuang <jikuang@umd.edu>
2021-06-11 22:21:49 -0500

    expand GetAnalysis script

--- src/ufs_workflow/tasks/GetAnalysis.py
+++ src/ufs_workflow/tasks/GetAnalysis.py
@@ -3,5 +3,10 @@
 import sys
 import os
 import r2d2
+from solo.logger import Logger
+from ewok.runtime import Configuration
+
+logger = Logger(os.environ['EWOK_TASK'])
+config = Configuration(sys.argv[1])
 
 print("I have to write my own GetAnalysis")
