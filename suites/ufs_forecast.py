edf0d58d9ebf84c72fcbafae39468186ef358dac
Claude Gibert <claude.gibert@gmail.com>
2021-08-01 18:02:50 +0100

    making progress

--- suites/ufs_forecast.py
+++ suites/ufs_forecast.py
@@ -1,4 +1,5 @@
 from ewok.suite import Suite
+from ewok.iterators import Iterator
 
 
 class UFSForecast:
@@ -12,11 +13,17 @@ class UFSForecast:
             start = 'config.init_cycle'
             stop = 'config.last_cycle'
             step = 'config.step_cycle'
-            with suite.add('fcCycle', start=start, stop=stop, step=step) as cycle:
-                analysis = cycle.add('GetAnalysis')
-                prep = cycle.add('PrepareRun', stage, analysis)
-                fc = cycle.add('Forecast', setup, analysis, prep)
-                archive = cycle.add('SaveForecast', fc)
-                cycle.add('CleanCycles', archive, )
-            suite.add('FinishExperiment', cycle, defstatus='suspended')
+            with suite.add('fcCycle', loop_type='serial',
+                           loop_var='DATE', iterator=Iterator('Date', start, stop, step)) as cycle:
+                cycle.add('StartCycle')
+                with cycle.add('Models', loop_type='parallel',
+                               loop_var='MODEL', iterator=Iterator('Models')) as models:
+                    analysis = models.add('GetAnalysis')
+                    prep = models.add('PrepareRun', stage, analysis)
+                    fc = models.add('Forecast', setup, analysis, prep)
+                    archive = models.add('SaveForecast', fc)
+                cycle.add('EndCycle', models)
+                cycle.add('CleanCycles', models)
+            finish = suite.add('FinishExperiment', cycle, defstatus='suspended')
+            suite.add('CleanupExperiment', finish, defstatus='suspended')
         return suite
