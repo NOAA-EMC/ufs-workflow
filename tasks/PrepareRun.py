ecb99dbaeff49cf5d227fd0c0b4bebfcfecf5c98
Claude Gibert <claude.gibert@gmail.com>
2021-08-03 22:16:57 +0100

    progress

---
+++ tasks/PrepareRun.py
@@ -0,0 +1,17 @@
+import sys
+import os
+from solo.logger import Logger
+from solo.stage import Stage
+from ewok.runtime import Configuration
+
+
+logger = Logger(os.environ['EWOK_TASK'])
+config = Configuration(sys.argv[1], silent=True)
+
+model_info = config.get_model_info()
+
+if 'STAGE_RUN' in model_info:
+    logger.info('run staging data found, processing it')
+    for staging, origin in zip(model_info['STAGE_RUN'], model_info['STAGE_RUN_origin']):
+        path = os.path.dirname(origin)
+        stage = Stage(path, config.stage_dir, staging)
--- tasks/PrepareRun.sh
+++
@@ -1,5 +0,0 @@
-set -eux
-
-eval $(ewok_source_config $CONFIG_FILE current_dir stage_dir)
-mkdir -p $current_dir || true
-cp -r $stage_dir/* $current_dir
