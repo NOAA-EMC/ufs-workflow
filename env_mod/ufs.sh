c19cc2b0b0bc7be1f060b494c117d65cea58540d
Travis Sluka <tsluka@ucar.edu>
2021-08-19 14:21:42 -0500

    env file

--- env_mod/ufs.sh
+++ env_mod/ufs.sh
@@ -1,1 +1,14 @@
-echo "INSERT THE UFS MODULES/ENVIRONMENT HERE"
+echo "sourcing the UFS forecast environment"
+
+set +x
+source ./module-setup.sh
+module use .
+module load modules.ufs_model
+module list
+set -x
+
+ulimit -s unlimited
+
+export OMP_STACKSIZE=512M
+export KMP_AFFINITY=scatter
+export OMP_NUM_THREADS=1
